6c386caec7db45cf0911d93b2bc9cea2e3995fce
abdulsaheef <abdulshaeef313@gmail.com>
2025-06-21 19:03:08 +0400

    Create compass.js

---
+++ projects/digital-compass/compass.js
@@ -0,0 +1,17 @@
+const compass = document.getElementById('compass');
+const headingText = document.getElementById('heading');
+
+window.addEventListener('deviceorientationabsolute', handleOrientation, true);
+window.addEventListener('deviceorientation', handleOrientation, true);
+
+function handleOrientation(event) {
+  const heading = event.alpha;
+
+  if (typeof heading === 'number') {
+    const rotation = 360 - heading;
+    compass.style.transform = `rotate(${rotation}deg)`;
+    headingText.textContent = `${Math.round(heading)}°`;
+  } else {
+    headingText.textContent = 'Compass not supported';
+  }
+}
