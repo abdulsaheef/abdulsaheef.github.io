9496da1e9ff1a6791f8fa18216c4cc2e313f72a3
abdulsaheef <abdulshaeef313@gmail.com>
2025-06-22 03:31:41 +0400

    Update projects.js

--- projects.js
+++ projects.js
@@ -45,6 +45,7 @@ customProjects.forEach(project => {
   card.setAttribute("data-aos", "fade-up");
 
   card.innerHTML = `
+    <img src="${project.image}" alt="${project.title}" class="project-thumb">
     <h3>${project.title}</h3>
     <p>${project.description}</p>
     <a href="${project.link}" target="_blank" class="btn">Launch</a>
