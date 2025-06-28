c5b1cf550f01ae52a4f36839dde4d823ff7913c1
abdulsaheef <abdulshaeef313@gmail.com>
2025-06-29 03:18:55 +0400

    Update script.js

--- projects/duebook/script.js
+++ projects/duebook/script.js
@@ -302,12 +302,13 @@ function removeVendor(index) {
 }
 
 // Calendar
-function renderCalendar() {
+function renderCalendar(monthOffset = 0) {
   if (!calendarGrid) return;
   
   calendarGrid.innerHTML = '';
   
   const today = new Date();
+  today.setMonth(today.getMonth() + monthOffset);
   const currentMonth = today.getMonth();
   const currentYear = today.getFullYear();
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
